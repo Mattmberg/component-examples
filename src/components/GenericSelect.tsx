f9417ddf84b4025401240a0f88f3bb7a07a46565
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2023-04-10 19:00:04 -0400

    Added Express, Mongodb, CRUD for Students

--- src/components/GenericSelect.tsx
+++ src/components/GenericSelect.tsx
@@ -9,19 +9,19 @@ onChange: (value: TValue) => void;
 };
 
 export const GenericSelect = <TValue extends BaseType>({ values, onChange }: GenericSelectProps<TValue>) => {
-const onSelectChange = (e: any) => {
-    const val = values.find((value) => value.id === e.target.value);
+    const onSelectChange = (e: any) => {
+        const val = values.find((value) => value.id === e.target.value);
 
-    if (val) onChange(val);
-};
+        if (val) onChange(val);
+    };
 
-return (
-    <select onChange={onSelectChange}>
-    {values.map((value) => (
-        <option key={value.id} value={value.id}>
-        {value.title}
-        </option>
-    ))}
-    </select>
-);
+    return (
+        <select onChange={onSelectChange}>
+        {values.map((value) => (
+            <option key={value.id} value={value.id}>
+            {value.title}
+            </option>
+        ))}
+        </select>
+    );
 };
